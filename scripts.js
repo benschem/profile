777cb189834bd3ed9ae709eab705b930c746d9c9
Ben Schembri <benschembri@gmail.com>
2024-09-02 18:22:21 +1000

    fixed errors

--- scripts.js
+++ scripts.js
@@ -1,22 +1,58 @@
-// Feather icons
-feather.replace();
+// Example usage:
+// Insert this line in the <head> of your index.html file after any external dependencies:
+// <script src="/scripts.js" defer></script>
+// Make sure to include the defer keyword.
 
-// Dark mode toggle switch
-const toggle = document.querySelector("#dark-mode-toggle");
-const switchIcon = document.querySelector("#switch-icon");
-const root = document.querySelector(":root");
-let darkMode;
+// Listen for when the HTML document has been completely parsed and all deferred scripts have downloaded and executed.
+document.addEventListener("DOMContentLoaded", (event) => {
+  // The DOMContentLoaded event is queued before stylesheets, images, subframes, and async scripts finish loading,
+  // but after deferred scripts, which do wait for stylesheets to load, meaning your own javascript will run last.
 
-if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
-  darkMode = true;
-  switchIcon.style.fill = "none";
-} else {
-  darkMode = false;
-  switchIcon.style.fill = "yellow";
-}
+  // Load Feather icons.
+  feather.replace();
 
-toggle.addEventListener("click", () => {
-  if (darkMode) {
+  // Select dark mode toggle switch.
+  const toggle = document.querySelector("#dark-mode-toggle");
+  const switchIcon = document.querySelector("#switch-icon");
+
+  // Detect browser dark mode preference and style dark mode toggle switch icon to match.
+  const theBrowserPrefersDarkMode = () => {
+    return window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches;
+  };
+
+  let darkMode;
+  if (theBrowserPrefersDarkMode()) {
+    darkMode = true;
+    switchIcon.style.fill = "none";
+  } else {
+    darkMode = false;
+    switchIcon.style.fill = "var(--color-light)";
+  }
+
+  // When the icon is clicked, toggle dark mode
+  toggle.addEventListener("click", () => {
+    if (darkMode) {
+      turnOffDarkMode();
+    } else {
+      turnOnDarkMode();
+    }
+  });
+
+  const turnOffDarkMode = () => {
+    applyLightColors();
+    darkMode = false;
+    switchIcon.style.fill = "var(--color-light)";
+  };
+
+  const turnOnDarkMode = () => {
+    applyDarkColors();
+    darkMode = true;
+    switchIcon.style.fill = "none";
+  };
+
+  const root = document.querySelector(":root");
+
+  const applyLightColors = () => {
     root.style.setProperty("--color-alt", "var(--purple-6)");
     root.style.setProperty("--color-alt-light", "var(--purple-2)");
     root.style.setProperty("--color-light", "var(--orange-3)");
@@ -25,9 +61,9 @@ toggle.addEventListener("click", () => {
     root.style.setProperty("--background", "var(--purple-0)");
     root.style.setProperty("--background-alt", "var(--purple-8)");
     root.style.setProperty("--body-text", "black");
-    darkMode = false;
-    switchIcon.style.fill = "var(--color-light)";
-  } else {
+  };
+
+  const applyDarkColors = () => {
     root.style.setProperty("--color-alt", "var(--purple-6)");
     root.style.setProperty("--color-alt-light", "var(--purple-2)");
     root.style.setProperty("--color-light", "var(--orange-3)");
@@ -36,17 +72,28 @@ toggle.addEventListener("click", () => {
     root.style.setProperty("--background", "var(--purple-12)");
     root.style.setProperty("--background-alt", "var(--purple-10)");
     root.style.setProperty("--body-text", "var(--purple-0)");
-    darkMode = true;
-    switchIcon.style.fill = "none";
-  }
-});
+  };
 
-// Scroll down arrow
-const downArrow = document.querySelector("#down-arrow");
-window.addEventListener("scroll", () => {
-  if (window.scrollY === 0) {
+  // As soon as you scroll, hide the bouncing arrow encouraging you to scroll down.
+  window.addEventListener("scroll", () => {
+    if (pageIsScrolledAllTheWayUp()) {
+      showArrow();
+    } else {
+      hideArrow();
+    }
+  });
+
+  const pageIsScrolledAllTheWayUp = () => {
+    return window.scrollY < 40;
+  };
+
+  const downArrow = document.querySelector("#down-arrow");
+
+  const showArrow = () => {
     downArrow.style.display = "block";
-  } else {
+  };
+
+  const hideArrow = () => {
     downArrow.style.display = "none";
-  }
+  };
 });
